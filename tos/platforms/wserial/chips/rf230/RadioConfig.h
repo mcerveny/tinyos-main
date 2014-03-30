790f79636d25023b854ca474e1ecde979d64de47
Martin Cerveny <M.Cerveny@computer.org>
2014-03-30 12:45:21 +0200

    use channel 26 on all radios

--- tos/platforms/wserial/chips/rf230/RadioConfig.h
+++ tos/platforms/wserial/chips/rf230/RadioConfig.h
@@ -56,7 +56,7 @@ enum
 
 /* This is the default value of the CHANNEL field of the PHY_CC_CCA register. */
 #ifndef RF230_DEF_CHANNEL
-#define RF230_DEF_CHANNEL	11
+#define RF230_DEF_CHANNEL	23
 #endif
 
 /* The number of microseconds a sending MESHBEAN mote will wait for an acknowledgement */
